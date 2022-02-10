0c5f78b39fb772f8d47423f3e90ba87fb0589f14
goreleaser <trentm+goreleaser@gmail.com>
2022-02-09 21:59:57 -0800

    Brew formula update for go-ecslog version v0.6.0

--- Formula/ecslog.rb
+++ Formula/ecslog.rb
@@ -5,25 +5,25 @@
 class Ecslog < Formula
   desc "Pretty rendering of ecs-logging logs"
   homepage "https://github.com/trentm/go-ecslog"
-  version "0.5.0"
+  version "0.6.0"
   license "Apache-2.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/trentm/go-ecslog/releases/download/v0.5.0/ecslog_0.5.0_macos_x86_64.tar.gz"
-    sha256 "4696b5846b906ece70d7f1f5e629b92c33b693917518b9df84f13cec055ffb77"
+    url "https://github.com/trentm/go-ecslog/releases/download/v0.6.0/ecslog_0.6.0_macos_x86_64.tar.gz"
+    sha256 "6ac731144278dcfc4cb2cadd6dd6fa9e49c150de11acfde9a22173df9e402592"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/trentm/go-ecslog/releases/download/v0.5.0/ecslog_0.5.0_macos_arm64.tar.gz"
-    sha256 "c0a674a61ae2364c0354e43f556c9423bc137fe5df70db23a0bcffb9c5e4ec81"
+    url "https://github.com/trentm/go-ecslog/releases/download/v0.6.0/ecslog_0.6.0_macos_arm64.tar.gz"
+    sha256 "e48aaf86e1db4fcc92a35f91825f60eda0ab7e6916ddec7e35a04db981c29f71"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/trentm/go-ecslog/releases/download/v0.5.0/ecslog_0.5.0_linux_x86_64.tar.gz"
-    sha256 "9e35140bf6556596eb30bafe150328dfd033ed512d31b11b934e8c4bc4652d86"
+    url "https://github.com/trentm/go-ecslog/releases/download/v0.6.0/ecslog_0.6.0_linux_x86_64.tar.gz"
+    sha256 "f19b18049ae6303b2ace84cb7d12049779c1a91018b65924cc44d70868379621"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/trentm/go-ecslog/releases/download/v0.5.0/ecslog_0.5.0_linux_arm64.tar.gz"
-    sha256 "bb524f62147a76574614fec8f4cbf86a9bb8faf385759dec72c4fb4e2e724d1c"
+    url "https://github.com/trentm/go-ecslog/releases/download/v0.6.0/ecslog_0.6.0_linux_arm64.tar.gz"
+    sha256 "c8a980bc58438dcdf45ef6408db09ce374dff58973a32c91f04a26982f1f2160"
   end
 
   def install
